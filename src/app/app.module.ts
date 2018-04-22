fc72a4025c586d4e754b17a725e5f97e6671a187
DESKTOP-UQLAUR6\Centhil <centhil95@outlook.com>
2018-04-23 00:15:47 +0530

    comment form validation and temporary posting - done

---
+++ src/app/app.module.ts
@@ -0,0 +1,58 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import 'hammerjs';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishDetailsComponent } from './dish-details/dish-details.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishDetailsComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent
+  ],
+  entryComponents: [
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/dish-details/dish-details.component.ts
@@ -0,0 +1,119 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import 'rxjs/add/operator/switchMap';
+import { Dish } from '../shared/dish';
+import {Comment} from '../shared/comment';
+
+import { DishService } from '../services/dish.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-dish-details',
+  templateUrl: './dish-details.component.html',
+  styleUrls: ['./dish-details.component.scss']
+})
+export class DishDetailsComponent implements OnInit { 
+  dish: Dish;
+  dishIds: number[];
+  prev: number;
+  next: number;
+  commentForm: FormGroup;
+  comment: Comment;
+  rating: number;
+  date: string;
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required':      'Your Name is required.',
+      'minlength':     'Your Name must be at least 2 characters long.',
+      'maxlength':     'Your Name cannot be more than 25 characters long.'
+    },
+    'comment': {
+      'required':      'Comment is required.',
+      'minlength':     'Comment must be at least 2 characters long.',
+      'maxlength':     'Comment cannot be more than 25 characters long.'
+    }
+  };
+
+  constructor(private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private fb: FormBuilder) {
+      this.createForm();
+    }
+
+  ngOnInit() {
+    let id = +this.route.snapshot.params['id'];
+    this.dishService.getDish(id)
+      .subscribe(dish => this.dish = dish);
+
+    this.dishService.getDishIds()
+      .subscribe(dishIds => this.dishIds = dishIds);
+
+    this.route.params
+      .switchMap((params: Params) => this.dishService.getDish(+params['id']))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+  }
+
+  setPrevNext(dishId: number) {
+    let index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1)%this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1)%this.dishIds.length];
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  createForm(): void {
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      comment: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      rating: ['']
+    });
+    this.commentForm.valueChanges
+    .subscribe(data => this.onValueChanged(data));
+
+  this.onValueChanged(); // (re)set validation messages now
+
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  onInputChange(event: any) {
+
+  }
+
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    this.comment.date = new Date().toISOString();
+    console.log(this.comment);
+    this.dish.comments.push(this.comment);
+    this.commentForm.reset({
+      'author': '',
+      'rating': 5,
+      'comment': ''
+    });
+  }
+
+}
